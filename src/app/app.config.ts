6723c1d89552ecdbbe7b9609ecc57f3fbb9f1dc8
Stephanie <stephaniiehallberg@gmail.com>
2024-07-10 09:37:40 +0200

    feat: pagination

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideHttpClient(), provideAnimations()]
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-import { ProductsService } from '../services/products.service';
-import { Product } from '../../types';
-import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+import { ProductsService } from '../services/products.service';
+import { ProductComponent } from '../components/product/product.component';
+import { Product } from '../../types';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductComponent, CommonModule],
+  imports: [ProductComponent, CommonModule, PaginatorModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -19,16 +20,28 @@ export class HomeComponent {
 
   products: Product[] = [];
 
+  totalRecords: number = 0;
+  rows: number = 5;
+
   onProductOutput(product: Product) {
     console.log(product, "Output");
   }
 
-  ngOnInit() {
+  onPageChange(event: any) {
+    this.fetchProducts(event.page, event.rows);
+  }
+
+  fetchProducts(page: number, perPage: number) {
     this.productsService.getProducts('http://localhost:3000/clothes', {
-      page: 0,
-      perPage: 5
+      page,
+      perPage
     }).subscribe((products) => {
       this.products = products.items;
+      this.totalRecords = products.total;
     })
+  }
+
+  ngOnInit() {
+    this.fetchProducts(0, this.rows);
   }
 }
